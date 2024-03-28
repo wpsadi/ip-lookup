db47b0c2b4e6f74260d8f4406f6e312808f47c8b
Aditya <118204758+wpsadi@users.noreply.github.com>
2024-03-28 21:56:11 +0000

    save

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ import express from "express"
 
 import resolveIP from "./using Puppeteer/01 beginning/01.js"
 
+import {exec} from "child_process"
+
+// Replace 'ls' with the command you want to run
+let test = ()=>exec('npx puppeteer browsers install chrome', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Command stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Command output: ${stdout}`);
+});
+
+test()
+
 const app = express()
 
 app.use("/ip/:ip", async(req, res) => {
